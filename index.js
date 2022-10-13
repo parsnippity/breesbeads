1ba8e8c26fcdd9f93e992601dcbda754cdfdee44
Brianne Siemsen <briannesiemsen@gmail.com>
2022-10-13 16:58:03 -0700

    things

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("test");
+
+var test = [document.getElementById("customNecklace"), document.getElementById("customBracelet")];
+
+function custom() {
+    var yell = 0;
+    while(yell < test.length) {
+        if(test[yell].checked) {
+            document.getElementById("customColor").setAttribute("disabled", "false");
+        };
+        yell ++;
+    }
+}
