a0cfb401e7443f0a442fdba26492866d7946ac02
Brianne Siemsen <briannesiemsen@gmail.com>
2022-10-13 18:16:15 -0700

    edits

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ console.log("test");
 var test = [document.getElementById("customNecklace"), document.getElementById("customBracelet")];
 
 function custom() {
-    var yell = 0;
-    while(yell < test.length) {
-        if(test[yell].checked) {
-            document.getElementById("customColor").setAttribute("disabled", "false");
-        };
-        yell ++;
-    }
+    if(test[0].checked) {
+        document.getElementById("customColor").removeAttribute("disabled");
+    } else if(test[1].checked) {
+        document.getElementById("customColor").removeAttribute("disabled");
+    } else {
+        document.getElementById("customColor").setAttribute("disabled", "");
+    };
 }
