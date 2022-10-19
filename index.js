b3ebf06672aed6cdc259f54248c0eecaac3b7013
Brianne Siemsen <briannesiemsen@gmail.com>
2022-10-18 18:26:07 -0700

    hi

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-console.log("test");
-
 var test = [document.getElementById("customNecklace"), document.getElementById("customBracelet")];
+let list = document.getElementsByClassName("color");
 
 function custom() {
-    if(test[0].checked) {
-        document.getElementById("customColor").removeAttribute("disabled");
-    } else if(test[1].checked) {
+    if(test[0].checked || test[1].checked) {
         document.getElementById("customColor").removeAttribute("disabled");
     } else {
         document.getElementById("customColor").setAttribute("disabled", "");
+        for (var x = 0; x < list.length; x++) {
+            list[x].checked = false;
+        }
     };
+}
+
+var hi = document.getElementsByClassName('review');
+
+function all() {
+    console.log("hi");
 }
